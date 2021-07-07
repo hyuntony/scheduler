61836f93f5d539d344dca26d36e7089a7be3f5a5
hyuntony <ankim90@gmail.com>
2021-07-07 02:20:52 -0400

    complete form component

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
-            /*
-              This must be a controlled component
-            */
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
-        <InterviewerList value={props.interviewers} interviewer={props.interviewer} onChange={props.onCancel} />
+        <InterviewerList value={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
