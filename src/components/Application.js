237105a2f33f760375010fe3d0fad3f0450f1ca4
hyuntony <ankim90@gmail.com>
2021-07-07 23:43:18 -0400

    install axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList"
+import Appointment from "./Appointment/index"
 
 import "components/Application.scss";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Andres Murray",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Kevin Kim",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg", 
+      }
+    }
+  }
+
+];
 
 const days = [
   {
@@ -48,7 +98,13 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return <Appointment
+                    key={appointment.id}
+                    {...appointment}
+                  />
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
